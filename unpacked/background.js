3e3e5fec8ba9931a662736596f8de8790e1da1a0
Evan Hahn <me@evanhahn.com>
2012-10-02 11:05:19 -0400

    Add tooltip text

--- unpacked/background.js
+++ unpacked/background.js
@@ -7,14 +7,15 @@ window.onload = function() {
 
 		// Everything's all good!
 		if (since < Pingaling.BAD_START) {
-			chrome.browserAction.setIcon({ path: "good.png" });
+			chrome.browserAction.setIcon({ path: 'good.png' });
+			chrome.browserAction.setBadgeText({ text: '' });
 		}
 
 		// Not so good
 		else {
-			chrome.browserAction.setIcon({ path: "bad.png" });
+			chrome.browserAction.setIcon({ path: 'bad.png' });
 			chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-			chrome.browserAction.setBadgeText({ text: since });
+			chrome.browserAction.setBadgeText({ text: ('' + since) });
 		}
 
 	}, 500);
